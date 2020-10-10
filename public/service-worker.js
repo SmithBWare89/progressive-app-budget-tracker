20f5360e8e12ca1ea51e64253713a556c7cf23dc
Stephon Smith <smithwrestling89@gmail.com>
2020-10-09 21:34:37 -0400

    removed webpack

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,16 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
     "./index.html",
-    "./"
+    "./assets/css/styles.css",
+    "./assets/js/index.js",
+    "./assets/icons/icon-72x72.png",
+    "./assets/icons/icon-96x96.png",
+    "./assets/icons/icon-128x128.png",
+    "./assets/icons/icon-144x144.png",
+    "./assets/icons/icon-152x152.png",
+    "./assets/icons/icon-192x192.png",
+    "./assets/icons/icon-384x384.png",
+    "./assets/icons/icon-512x512.png",
 ];
 
 // Respond with cached resources
